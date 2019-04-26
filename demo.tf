ff093a185cfc7fb465aa90f54d09caeb6d7227f3
Étienne Brouillard <etienne.brouillard@outlook.com>
2019-04-26 13:12:44 -0400

    example with a public subnet

--- demo.tf
+++ demo.tf
@@ -10,13 +10,51 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_vpc" "tf_network" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = "${aws_vpc.tf_network.id}"
+}
+
+resource "aws_subnet" "subnet_subnet_a" {
+  vpc_id                  = "${aws_vpc.tf_network.id}"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  depends_on              = ["aws_internet_gateway.gateway"]
+}
+
+resource "aws_subnet" "subnet_subnet_b" {
+  vpc_id                  = "${aws_vpc.tf_network.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  depends_on              = ["aws_internet_gateway.gateway"]
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = "${aws_vpc.tf_network.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gateway.id}"
+  }
+}
+
+resource "aws_route_table_association" "public_route_assoc" {
+  subnet_id      = "${aws_subnet.subnet_subnet_a.id}"
+  route_table_id = "${aws_route_table.public_rt.id}"
+}
+
 resource "aws_key_pair" "edgenda_key" {
   key_name   = "ec2_instance_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDB+DbV+bzONPPcIDl9iyMVgd0B553nL4qiDnvYBXft8JCDquyA4Xci8mhKRCh/CXgu/HU0ndB0gwqnS1CCK5vMD56yGq9kN9ZKaCZjpgwb2D2d1+geMWlfTbG3c5+8EOHHrGsIxcunEhutpQrQHq++4RCnBfXiEkbKuzayYLxxU+nCIDbxBteTn9Z47xvHmeh1M/pjD94BzNMiPvazOs5JbP2jTiCpiFpVoBK1wxKPb2VaKj8vKcQ0bztggc+t+LAA8yalh6uSqeGE7Pvw2jmaD/2LO3UMOaDYn3CogKLGFoI7zMUOXBr5oFUvo6s+SyyCOI9QkrDFCIKku8DNCaj+Sg/YqUDoUSixidCVlRdayAhyUpQeTvuORNTIhcSCgZyqJPNg6Ja8w6SeO6Tf2qrbqsjYWNBrFVKHqd8nFg1M2WDQBlLlEgI5xDsmYIn+tKLtTSiU+da/jbr0MNu0EINAoP5jcfte+9BGJGQ4/W0y0Gp2LL5Pj4aNJrSsXRlx1YL1I3P416pHpI7qUuLDnjVhBIZWhTl9g8lDOpZh41W1By9WHs9S5lduHL1CyG36HewbY1hrzcbH+p0pMtvLSFkToIvMVTzMveFm4cvw5fbsNyjRX7umXGg33qkie2fQjgdT7Tt10p49R1YbKyT6k6Tz368BFI2j2qnTWmqf98iJ0w== efog@STRONGBAD10"
 }
 
 resource "aws_security_group" "default_security_group_ipv4" {
-  name = "Ensure_SSH_Allow_All_IPV4"
+  vpc_id = "${aws_vpc.tf_network.id}"
+  name   = "Ensure_SSH_Allow_All_IPV4"
 
   ingress {
     to_port     = 22
@@ -38,5 +76,19 @@ resource "aws_instance" "micro_vm" {
   key_name      = "${aws_key_pair.edgenda_key.key_name}"
   instance_type = "t2.micro"
 
-  security_groups = ["${aws_security_group.default_security_group_ipv4.name}"]
+  subnet_id                   = "${aws_subnet.subnet_subnet_a.id}"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = [
+    "${aws_security_group.default_security_group_ipv4.id}"]
+}
+
+resource "aws_eip" "ip" {
+  instance                  = "${aws_instance.micro_vm.id}"
+  vpc                       = true
+  associate_with_private_ip = "${aws_instance.micro_vm.private_ip}"
+  depends_on                = ["aws_internet_gateway.gateway"]
+}
+
+output "ip" {
+  value = "${aws_instance.micro_vm.public_ip}"
 }
